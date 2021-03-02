0645524d2a10bd52c020821fb4b947f8c9d12ab7
carlosjavejapan <javedavalos@gmail.com>
2021-03-02 21:52:57 +0900

    slideshow react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Slideshow, Slide, TextSlide} from './componentes/Slideshow';
+import './style.css';
+import styled from 'styled-components';
+import img1 from './img/1.jpg';
+import img2 from './img/2.jpg';
+import img3 from './img/3.jpg';
+import img4 from './img/4.jpg';
+import img5 from './img/5.jpg';
+import img6 from './img/6.jpg';
+import img7 from './img/7.jpg';
+import img8 from './img/8.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <main>
+            <Titulo>Productos en descuento</Titulo>
+            <Slideshow controles={true}>
+                <Slide>
+                    <a href="https://carlosjavejapan.github.io/2021-portfolio/">
+                        <img src={img1} alt="foto1"/>
+                    </a>
+                    <TextSlide colorFondo='rgb(129 49 43)' colorTexto='#fff'>
+                        <p>20% descuento en productos Amazon</p>
+                    </TextSlide>
+                </Slide>
+                <Slide>
+                    <a href="https://carlosjavejapan.github.io/2021-portfolio/">
+                        <img src={img2} alt="foto1"/>
+                    </a>
+                    <TextSlide colorFondo='rgb(85 127 157)' colorTexto='#fff'>
+                        <p>20% descuento en productos Amazon</p>
+                    </TextSlide>
+                </Slide>
+                <Slide>
+                    <a href="https://carlosjavejapan.github.io/2021-portfolio/">
+                        <img src={img3} alt="foto1"/>
+                    </a>
+                    <TextSlide colorFondo='rgb(60 66 74)' colorTexto='#fff'>
+                        <p>20% descuento en productos Amazon</p>
+                    </TextSlide>
+                </Slide>
+                <Slide>
+                    <a href="https://carlosjavejapan.github.io/2021-portfolio/">
+                        <img src={img4} alt="foto1"/>
+                    </a>
+                    <TextSlide colorFondo='rgb(214 189 218)' colorTexto='#fff'>
+                        <p>20% descuento en productos Amazon</p>
+                    </TextSlide>
+                </Slide>
+            </Slideshow>
+
+            <Titulo>Productos en descuento 2</Titulo>
+            <Slideshow controles={true}  autoplay={true} velocidad={500} intervalo={4000}>
+                <Slide>
+                    <a href="https://carlosjavejapan.github.io/2021-portfolio/">
+                        <img src={img5} alt="foto1"/>
+                    </a>
+                    <TextSlide>
+                        <p>20% descuento en productos Amazon</p>
+                    </TextSlide>
+                </Slide>
+                <Slide>
+                    <a href="https://carlosjavejapan.github.io/2021-portfolio/">
+                        <img src={img6} alt="foto1"/>
+                    </a>
+                    <TextSlide>
+                        <p>20% descuento en productos Amazon</p>
+                    </TextSlide>
+                </Slide>
+                <Slide>
+                    <a href="https://carlosjavejapan.github.io/2021-portfolio/">
+                        <img src={img7} alt="foto1"/>
+                    </a>
+                    <TextSlide>
+                        <p>20% descuento en productos Amazon</p>
+                    </TextSlide>
+                </Slide>
+                <Slide>
+                    <a href="https://carlosjavejapan.github.io/2021-portfolio/">
+                        <img src={img8} alt="foto1"/>
+                    </a>
+                    <TextSlide>
+                        <p>20% descuento en productos Amazon</p>
+                    </TextSlide>
+                </Slide>
+            </Slideshow>
+        </main>
+    );
 }
 
+const Titulo = styled.p`
+    font-size: 18px;
+    font-weight: 700;
+    text-transform: uppercase;
+    margin-bottom: 10px;
+    margin-top: 40px;
+`;
+ 
 export default App;
---
+++ src/componentes/Slideshow.js
@@ -0,0 +1,170 @@
+import React, {useRef, useEffect, useCallback} from 'react';
+
+import {ReactComponent as FlechaIzquierda} from "./../img/left-thin.svg";
+import {ReactComponent as FlechaDerecha} from "./../img/right-thin.svg";
+import styled from 'styled-components';
+
+const Slideshow = ({
+        children,
+        controles=false,
+        autoplay=false,
+        velocidad=500,
+        intervalo=5000
+    }) => {
+
+    const slideshow = useRef(null);
+    const intervalosSlideshow = useRef(null)
+
+    const siguiente = useCallback(() => {
+        // Comprobando que el slideshow tenga elementos
+        if (slideshow.current.children.length > 0) {
+            console.log('siguiente');
+            // Obtenemos el primer elemento del slidshow
+            const primerElemento = slideshow.current.children[0];
+
+            // Establecemos la transion para el slideshow
+            slideshow.current.style.transition = `${velocidad}ms ease-out all`;
+
+            const widthSlide = slideshow.current.children[0].offsetWidth;
+
+            // Mover Slideshow
+            slideshow.current.style.transform = `translateX(-${widthSlide}px)`;
+
+            const transicion = () => {
+                // Reiniciamos la posicion del Slideshow.
+                slideshow.current.style.transition = 'none';
+                slideshow.current.style.transform = `translateX(0)`;
+
+                // Tomamos el primer elemento y lo mandamos a final.
+                slideshow.current.appendChild(primerElemento);
+
+                slideshow.current.removeEventListener('transitionend',transicion)
+            }
+
+            // EventListener para cuando termina la animacion
+            slideshow.current.addEventListener('transitionend', transicion);
+
+        }
+    },[velocidad]);
+
+
+    const anterior = () => {
+        console.log('anterior');
+
+        if (slideshow.current.children.length > 0) {
+            const index = slideshow.current.children.length - 1;
+            const ultimoElemento = slideshow.current.children[index];
+            slideshow.current.insertBefore(ultimoElemento, slideshow.current.firstChild);
+
+            slideshow.current.style.transition = 'none';
+
+            const widthSlide = slideshow.current.children[0].offsetWidth;
+            slideshow.current.style.transform = `translateX(-${widthSlide}px)`;
+
+            setTimeout(() => {
+                slideshow.current.style.transition = `${velocidad}ms ease-out all`;
+                slideshow.current.style.transform = `translateX(0)`;
+            }, 30);
+        }
+    }
+
+    useEffect(() => {
+        if (autoplay) {
+                intervalosSlideshow.current = setInterval(() => {
+                siguiente();
+            }, intervalo);
+
+            slideshow.current.addEventListener('mouseenter', () => {
+                clearInterval(intervalosSlideshow.current);
+            });
+
+            slideshow.current.addEventListener('mouseleave', () => {
+                intervalosSlideshow.current = setInterval(() => {
+                    siguiente();
+                }, intervalo);
+            });
+        }
+        
+    }, [autoplay, intervalo, siguiente])
+
+    return (
+        <ContenedorPrincipal>
+            <ContenedorSlideShow ref={slideshow}>
+                {children}        
+            </ContenedorSlideShow>
+            
+            
+            {controles && <Controles>
+                <Boton onClick={anterior}>
+                    <FlechaIzquierda />
+                </Boton>
+                <Boton derecho onClick={siguiente}>
+                    <FlechaDerecha />
+                </Boton>
+            </Controles>}
+        </ContenedorPrincipal>
+    );
+}
+
+const ContenedorPrincipal = styled.div`
+    position: relative;
+`;
+const ContenedorSlideShow = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+`;
+
+const Slide = styled.div`
+    min-width: 100%;
+    overflow: hidden;
+    transition: .3s ease all;
+    z-index: 10;
+    position: relative;
+    img {
+        width: 100%;
+        vertical-align: top;
+    }
+`;
+const TextSlide = styled.div`
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    background: ${props => props.colorFondo ? props.colorFondo : 'rgba(0,0,0,0.2)'};
+    text-align: center;
+    color: #fff;
+`;
+
+const Controles = styled.div`
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    position: absolute;
+    top: 0;
+    z-index: 20;
+`;
+
+const Boton = styled.button`
+    pointer-events: all;
+    background: none;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    width: 50px;
+    height: 100%;
+    position: absolute;
+    transition: .3s ease all;
+    &:hover {
+        background: rgba(0,0,0, 0.3);
+        path {
+            fill: #fff;
+        }
+    }
+
+    path {
+        filter: ${props => props.derecho ? 'drop-shadow(-2px 0px 0px #fff)' : 'drop-shadow(2px 0px 0px #fff)' }
+    }
+
+    ${props => props.derecho ? 'right: 0' : 'left: 0'}
+`;
+ 
+export {Slideshow, Slide, TextSlide};
